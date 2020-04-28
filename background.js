f1319ccdbc13572b401e613da8009cd8aa878c4c
Volkan Akin <Volkan.Akin@holidaycheck.com>
2020-04-28 19:05:18 +0200

    init

---
+++ background.js
@@ -0,0 +1,79 @@
+if (!localStorage.getItem("install_time")) {
+  var now = new Date().getTime();
+  localStorage.setItem("install_time", now);
+  chrome.tabs.create({
+    url: "https://paradify-web.azurewebsites.net/home/Installed",
+  });
+}
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  switch (message.type) {
+    case "setBadgeText":
+      setBadgeText(message.text);
+      break;
+    case "clearBadge":
+      clearBadge();
+      break;
+  }
+});
+
+function setBadgeText(text) {
+  chrome.browserAction.setBadgeText({ text: "Hey" });
+  chrome.browserAction.setBadgeBackgroundColor({ color: "#FF55A9" });
+
+  var intervalID = setInterval(function () {
+    chrome.browserAction.setBadgeText({ text: text });
+    window.clearInterval(intervalID);
+  }, 2000);
+}
+
+function clearBadge() {
+  chrome.browserAction.setBadgeText({ text: "" });
+}
+
+//When the page is loaded on youtube, this will let content.js know to show a notification.
+chrome.webNavigation.onCompleted.addListener(function (details) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    if (tabs.length == 0) return;
+
+    chrome.tabs.sendMessage(
+      tabs[0].id,
+      { type: "backgroundStarts", details: details },
+      function (response) {}
+    );
+  });
+});
+
+function contextMenuClicked(details) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    if (tabs.length == 0) return;
+
+    chrome.tabs.sendMessage(
+      tabs[0].id,
+      { type: "contextMenuClicked", details: details },
+      function (returnUrl) {
+        chrome.tabs.create({ url: returnUrl });
+      }
+    );
+  });
+}
+
+chrome.contextMenus.create({
+  title: "Add to Spotify Playlist",
+  contexts: ["selection"],
+  onclick: contextMenuClicked,
+});
+
+function getQueryStringValue(url, key) {
+  return decodeURIComponent(
+    url.replace(
+      new RegExp(
+        "^(?:.*[&\\?]" +
+          encodeURIComponent(key).replace(/[\.\+\*]/g, "\\$&") +
+          "(?:\\=([^&]*))?)?.*$",
+        "i"
+      ),
+      "$1"
+    )
+  );
+}
---
+++ content.js
@@ -0,0 +1,60 @@
+﻿chrome.runtime.onMessage.addListener(function (message, sender, sendResponse){
+    if (message != undefined ) {
+        if (message.type == 'backgroundStarts' && message.details != undefined && message.details.url == window.location.href) {
+            paradify.pageLoad();
+        } else if (message.type == 'getTrackInfo') {
+            var trackInfo = paradify.getTrackInfo(message.pageName);
+            sendResponse(trackInfo);
+        } else if (message.type == 'contextMenuClicked') {
+            var returnUrl = paradify.contextMenuClicked(message.details.selectionText);
+            sendResponse(returnUrl);
+        }
+    }
+});
+
+var paradify = {
+    pageLoad : function() {
+        chrome.runtime.sendMessage({type: 'clearBadge'});
+        var url = window.location.href.toLowerCase();
+        var pageName = getPageName(url);
+        if (pageName != undefined) {
+            var trackInfo = paradify.getTrackInfo(pageName);
+            if (trackInfo != undefined && trackInfo.success) {
+                chrome.runtime.sendMessage({type: 'setBadgeText', text: ' 1 '});
+            }
+        }
+    },
+
+    getTrackInfo : function(pageName) {
+
+        var response = {};
+
+        try {
+            var playingResult = readNowPlayingText(pageName);
+
+            if (playingResult != null) {
+                response.track = playingResult.track;
+                response.artist = playingResult.artist;
+                response.success = true;
+            } else {
+                response.success = false;
+                response.errMessage = "There is no playing song on this page";
+            }
+        } catch (err) {
+            response.success = false;
+            response.errMessage = err.message;
+        }
+
+        response.pageName = pageName;
+
+        return response;
+    },
+
+    contextMenuClicked : function (text) {
+
+        var url = String.format("{0}{1}?q={2}", defaults.url, defaults.searchPath, text);
+
+        return url;
+
+    }
+}
