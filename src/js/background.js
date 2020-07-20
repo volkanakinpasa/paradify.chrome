5c221f30748b9101ed81f8ecbbbcd64351e9cc4b
volki <volkanakintr@gmail.com>
2020-07-20 21:17:15 +0200

    Extension2 (#1)
    * Add all in extension popup & Add reactjs

---
+++ src/js/background.js
@@ -0,0 +1,56 @@
+import '../img/16.png';
+import '../img/48.png';
+import '../img/128.png';
+
+const onTokenCompleted = (token) => {
+  chrome.storage.sync.set({ token: token });
+};
+
+function setBadgeText(text) {
+  chrome.browserAction.setBadgeText({ text });
+  setTimeout(() => clearBadge(), 5000);
+}
+
+function clearBadge() {
+  chrome.browserAction.setBadgeText({ text: '' });
+}
+
+const messageListener = (message, serder, callback) => {
+  switch (message.type) {
+    case 'onPopupOpened':
+      console.log(message.data);
+      break;
+    case 'clearBadge':
+      clearBadge();
+      callback('OK');
+    case 'setBadgeText':
+      setBadgeText(message.data);
+      callback('OK');
+
+      break;
+    case 'onTokenCompleted':
+      onTokenCompleted(message.data.token, callback);
+      break;
+  }
+};
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  messageListener(message, sender, sendResponse);
+});
+
+chrome.runtime.onMessageExternal.addListener(function (message, sender) {
+  messageListener(message, sender, () => {});
+});
+
+chrome.runtime.onInstalled.addListener(function (details) {
+  if (details.reason == 'install') {
+    chrome.tabs.create({
+      url: 'http://www.paradify.com/home/Installed',
+    });
+  }
+});
+
+var div = document.createElement('div');
+div.cssText =
+  'position: absolute: width: 200px; height: 200px; z-index: 99999; right: 0; top: 0';
+div.innerText = 'PARADIFY';
+document.body.appendChild(div);
---
+++ src/js/popup.js
@@ -0,0 +1,6 @@
+import '../css/popup.css';
+import Index from './Popup/Index.jsx';
+import React from 'react';
+import { render } from 'react-dom';
+
+render(<Index />, window.document.getElementById('app-container'));
